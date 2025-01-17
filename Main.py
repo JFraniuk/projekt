af03d5a81754d32644865383c52fbe216722e2f9
4 <jeremibarcinski@gmail.com>
2025-01-17 15:49:36 +0100

    add menu
    add menu functionality, modify pve and pvp to no longer run the function inside the file, add instalacja,bat script (not functional yet)

--- Main.py
+++ Main.py
@@ -1,1 +1,50 @@
-#siema tu maciek
+import argparse
+import PvP
+import PvE
+import EvP
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-t", "--tryb", help="Podaj tryb gry: PvP, PvE, EvP")
+parser.add_argument("-n", "--nick1", help = "Podaj nick gracza")
+parser.add_argument("-n2", "--nick2", help = "Podaj nick drugiego gracza")
+
+args = parser.parse_args() # parsowanie argumentow podanych przez uzytkownika
+
+trybGry = args.tryb
+nick1 = args.nick1
+nick2 = args.nick2
+
+if trybGry == None:
+    print("Witaj w świecie, bla bla bla wybierz opcję gry\n1.gracz vs gracz\n2.gracz vs komputer\n3.komputer vs gracz")
+    wybor = input()
+
+    while True:
+        match wybor:
+            case "1":
+                trybGry = "PvP"
+                break
+            case "2":
+                trybGry = "PvE"
+                break
+            case "3": 
+                trybGry = "EvP"
+                break
+            case _:
+                print("Niewlasciwy tryb gry, podaj jeszcze raz:")
+                wybor = input()
+
+print(trybGry)
+if nick1 == None:
+    nick1 = input("Podaj swój nick: ")
+
+if trybGry == "PvP" and nick2 == None:
+    nick2 = input("Podaj nick drugiego gracza: ")
+
+match trybGry:
+    case "PvP":
+        PvP.pvp()
+    case "PvE":
+        PvE.PvE()
+    case "EvP":
+        pass #TODO
